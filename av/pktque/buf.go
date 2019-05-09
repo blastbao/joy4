2bf59603a31b8eecf98a88717e340791b9c299ef
Jianwei Li <jianwei.li@shopee.com>
2019-05-10 00:17:03 +0800

    add comment

--- av/pktque/buf.go
+++ av/pktque/buf.go
@@ -17,44 +17,65 @@ func NewBuf() *Buf {
 	}
 }
 
+// 取出头部元素
 func (self *Buf) Pop() av.Packet {
 	if self.Count == 0 {
 		panic("pktque.Buf: Pop() when count == 0")
 	}
-
+	// i 为 self.Head 对应的元素下标
 	i := int(self.Head) & (len(self.pkts) - 1)
+	// 取出 self.Head 对应的消息 pkt
 	pkt := self.pkts[i]
+	// 重置 self.Head 消息（让旧的 Packet 被 GC 清理掉，以释放资源）
 	self.pkts[i] = av.Packet{}
+	// 总字节总数目更新
 	self.Size -= len(pkt.Data)
+	// 下标后移
 	self.Head++
+	// 数目减 1
 	self.Count--
-
+	// 返回 pkt
 	return pkt
 }
 
+// 扩容，相当于 reallocate
 func (self *Buf) grow() {
+	// 扩容
 	newpkts := make([]av.Packet, len(self.pkts)*2)
+	// 复制
 	for i := self.Head; i.LT(self.Tail); i++ {
 		newpkts[int(i)&(len(newpkts)-1)] = self.pkts[int(i)&(len(self.pkts)-1)]
 	}
+	// 重新赋值
 	self.pkts = newpkts
 }
 
+// 插入元素到尾部
 func (self *Buf) Push(pkt av.Packet) {
+
+	// 如果 len(self.pkts) 容量已经没有空余，需要扩容，这里调用 grow 扩大一倍，没有上限，可能有内存问题。
 	if self.Count == len(self.pkts) {
 		self.grow()
 	}
+
+	// 将新 pkt 放到队列 tail 位置
 	self.pkts[int(self.Tail)&(len(self.pkts)-1)] = pkt
+
+	// 更新指针、计数、字节总数
 	self.Tail++
 	self.Count++
 	self.Size += len(pkt.Data)
+
 }
 
+// 取出指定位置元素
 func (self *Buf) Get(pos BufPos) av.Packet {
+	// pkts[ pos % length ]
 	return self.pkts[int(pos)&(len(self.pkts)-1)]
 }
 
 func (self *Buf) IsValidPos(pos BufPos) bool {
+	// pos => [self.Head, self.Tail)
 	return pos.GE(self.Head) && pos.LT(self.Tail)
 }
 
--- av/pubsub/queue.go
+++ av/pubsub/queue.go
@@ -41,6 +41,7 @@ func NewQueue() *Queue {
 	return q
 }
 
+// 缓存 GOP 数目
 func (self *Queue) SetMaxGopCount(n int) {
 	self.lock.Lock()
 	self.maxgopcount = n
@@ -48,19 +49,18 @@ func (self *Queue) SetMaxGopCount(n int) {
 	return
 }
 
+// 设置 rtmp 流元数据
 func (self *Queue) WriteHeader(streams []av.CodecData) error {
 	self.lock.Lock()
-
 	self.streams = streams
 	for i, stream := range streams {
+		// 设置 视频流索引 videoidx 
 		if stream.Type().IsVideo() {
 			self.videoidx = i
 		}
 	}
 	self.cond.Broadcast()
-
 	self.lock.Unlock()
-
 	return nil
 }
 
@@ -83,11 +83,15 @@ func (self *Queue) Close() (err error) {
 func (self *Queue) WritePacket(pkt av.Packet) (err error) {
 	self.lock.Lock()
 
+	// pkt 入队
 	self.buf.Push(pkt)
+
+	// 若是视频帧、且是关键帧，则更新 GOP 计数
 	if pkt.Idx == int8(self.videoidx) && pkt.IsKeyFrame {
 		self.curgopcount++
 	}
 
+	// 如果当前缓存的 GOP 总数超过阈值，就 Pop 出冗余的视频帧
 	for self.curgopcount >= self.maxgopcount && self.buf.Count > 1 {
 		pkt := self.buf.Pop()
 		if pkt.Idx == int8(self.videoidx) && pkt.IsKeyFrame {
@@ -99,24 +103,33 @@ func (self *Queue) WritePacket(pkt av.Packet) (err error) {
 	}
 	//println("shrink", self.curgopcount, self.maxgopcount, self.buf.Head, self.buf.Tail, "count", self.buf.Count, "size", self.buf.Size)
 
+	// 信号量广播，唤醒所有阻塞在 ReadPacket() 函数中的协程
 	self.cond.Broadcast()
 
 	self.lock.Unlock()
 	return
 }
 
+
+
+
+
+
 type QueueCursor struct {
-	que    *Queue
-	pos    pktque.BufPos
-	gotpos bool
-	init   func(buf *pktque.Buf, videoidx int) pktque.BufPos
+	que    *Queue        // 指向底层 Queue 队列 
+	pos    pktque.BufPos // 读取 Queue 的位置下标
+	gotpos bool          // 默认值false，意味着 pos 尚未初始化
+	init   func(buf *pktque.Buf, videoidx int) pktque.BufPos // 初始化函数，用于设置 pos 初值
 }
 
+
+// 
 func (self *Queue) newCursor() *QueueCursor {
 	return &QueueCursor{
 		que: self,
 	}
 }
+
 
 // Create cursor position at latest packet.
 func (self *Queue) Latest() *QueueCursor {
@@ -124,6 +137,7 @@ func (self *Queue) Latest() *QueueCursor {
 	cursor.init = func(buf *pktque.Buf, videoidx int) pktque.BufPos {
 		return buf.Tail
 	}
+
 	return cursor
 }
 
@@ -173,16 +187,25 @@ func (self *Queue) DelayedGopCount(n int) *QueueCursor {
 	return cursor
 }
 
+
+
 func (self *QueueCursor) Streams() (streams []av.CodecData, err error) {
 	self.que.cond.L.Lock()
+
+	// 这个 for + wait 的作用是等待 self.que.streams 被赋值或者 self.que 被关闭
 	for self.que.streams == nil && !self.que.closed {
 		self.que.cond.Wait()
 	}
+
+	// for + wait 退出后: 
+	// 如果 self.que.streams 被赋值就取出并返回；
+	// 如果 self.que 被关闭就直接返回 err 。
 	if self.que.streams != nil {
-		streams = self.que.streams
+		streams = self.que.streams //设置返回值
 	} else {
 		err = io.EOF
 	}
+
 	self.que.cond.L.Unlock()
 	return
 }
@@ -190,28 +213,49 @@ func (self *QueueCursor) Streams() (streams []av.CodecData, err error) {
 // ReadPacket will not consume packets in Queue, it's just a cursor.
 func (self *QueueCursor) ReadPacket() (pkt av.Packet, err error) {
 	self.que.cond.L.Lock()
-	buf := self.que.buf
+	
+	// 指向底层 pkg 队列
+	buf := self.que.buf // *pktque.Buf
+
+	// 若 self.gotpos 值为 false（默认值），此时需要初始化 self.pos
 	if !self.gotpos {
+		// 设置 pos 的值
 		self.pos = self.init(buf, self.que.videoidx)
+		// 避免重复初始化
 		self.gotpos = true
 	}
+
 	for {
+
+		// 将读取指针 pos 修正到 [ buf.Head, ... , buf.Tail ] 之间
 		if self.pos.LT(buf.Head) {
 			self.pos = buf.Head
 		} else if self.pos.GT(buf.Tail) {
 			self.pos = buf.Tail
 		}
+
+		// 检查是否满足 buf.Head <= self.pos < buf.Tail 
 		if buf.IsValidPos(self.pos) {
+			// 取出 self.pos 位置元素 pkt
 			pkt = buf.Get(self.pos)
+			// 更新读取下标
 			self.pos++
+			// 退出循环，返回 pkg 
 			break
 		}
+
+		// 若经过修正之后有 self.pos = buf.Tail ，则会走到这里，此时意味着队列为空，
+		// 检查一下 self.que 是否被关闭，如果被关闭就直接返回 err
 		if self.que.closed {
 			err = io.EOF
 			break
 		}
+
+		// 至此，意味着 self.que 未关闭，但是当前没有新的可读数据，进入 wait 阻塞等待
 		self.que.cond.Wait()
 	}
+
+
 	self.que.cond.L.Unlock()
 	return
 }
